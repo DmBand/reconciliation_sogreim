d854e69a5ba4589977060a90aab6729affdc44d6
DmBand <d.bandysik@gmail.com>
2022-06-12 14:00:11 +0300

    reconciliation styles v1

--- reconciliation_app/apps.py
+++ reconciliation_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ReconciliationAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reconciliation_app'
+    verbose_name = 'сверка'
